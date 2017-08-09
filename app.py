0d82e2a2c215aa86f587239615dd3d06e48c1ec9
Travis Walentin <travis.walentin@gmail.com>
2017-08-09 02:02:31 -0700

    added database model and migrations

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object(os.environ["APP_SETTINGS"])
+print(os.environ["APP_SETTINGS"])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
+
 
 @app.route('/')
 def hello():
