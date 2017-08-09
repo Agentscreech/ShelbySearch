ed29803aa78696abde642cef578fc632945c331a
Travis Walentin <travis.walentin@gmail.com>
2017-08-09 00:37:13 -0700

    added env and config files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask
 app = Flask(__name__)
-
+app.config.from_object(os.environ["APP_SETTINGS"])
 
 @app.route('/')
 def hello():
@@ -10,6 +11,6 @@ def hello():
 @app.route("/<name>")
 def hello_name(name):
     return "Hello {}!".format(name)
-    
+
 if __name__ == "__main__":
     app.run()
